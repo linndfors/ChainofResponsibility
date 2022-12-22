15056642b8bbbc651d5f8eda0b86b5766fa2a4ad
Olli <91615532+linndfors@users.noreply.github.com>
2022-12-22 16:35:06 +0200

    Delete lab_12 directory

--- lab_12/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        Document document = new SmartDocument("gs://oop-course/Geico-2021.png");
-        TimedDocument info = new TimedDocument(document);
-        System.out.println(info.getTimePassed());
-    }
-}
